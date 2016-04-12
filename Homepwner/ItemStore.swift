99d4642c85cf593046c78a4db5173042cb2bc348
Daniel Aquino <daniel.j.aquino@gmail.com>
2016-04-11 21:19:29 -0500

    Finished chapter 10

--- Homepwner/ItemStore.swift
+++ Homepwner/ItemStore.swift
@@ -2,16 +2,25 @@ import Foundation
 
 class ItemStore {
     var allItems = [Item]()
-   
-    init() {
-        for _ in 0..<5 {
-            createItem()
-        }
-    }
     
     func createItem() -> Item {
         let newItem = Item(random: true)
         allItems.append(newItem)
         return newItem
+    }
+    
+    func remoteItem(item: Item) {
+        if let index = allItems.indexOf(item) {
+            allItems.removeAtIndex(index)
+        }
+    }
+    
+    func moveItemAtIndex(fromIndex: Int, toIndex: Int) {
+        if fromIndex == toIndex {
+            return
+        }
+        let movedItem = allItems[fromIndex]
+        allItems.removeAtIndex(fromIndex)
+        allItems.insert(movedItem, atIndex: toIndex)
     }
 }
--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -28,10 +28,6 @@ class ItemsViewController: UITableViewController {
         return cell
     }
     
-    @IBAction func addNewItem(sender: AnyObject) {
-        
-    }
-    
     @IBAction func toggleEditingMode(sender: AnyObject) {
         if editing {
             sender.setTitle("Edit", forState: .Normal)
@@ -41,6 +37,37 @@ class ItemsViewController: UITableViewController {
             sender.setTitle("Done", forState: .Normal)
             setEditing(true, animated: true)
         }
+    }
+    
+    @IBAction func addNewItem(sender: AnyObject) {
+        let newItem = itemStore.createItem()
+        if let index = itemStore.allItems.indexOf(newItem) {
+            let indexPath = NSIndexPath(forItem: index, inSection: 0)
+            tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+        }
+    }
+    
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            let item = itemStore.allItems[indexPath.row]
+            
+            let title = "Delete \(item.name)?"
+            let message = "Are you sure you want to delete this item?"
+            let ac = UIAlertController(title: title, message: message, preferredStyle: .ActionSheet)
+            let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+            ac.addAction(cancelAction)
+            let deleteAction = UIAlertAction(title: "Delete", style: .Destructive,
+                                             handler: { (action) in
+                self.itemStore.remoteItem(item)
+                self.tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+            })
+            ac.addAction(deleteAction)
+            presentViewController(ac, animated: true, completion: nil)
+        }
+    }
+    
+    override func tableView(tableView: UITableView, moveRowAtIndexPath sourceIndexPath: NSIndexPath, toIndexPath destinationIndexPath: NSIndexPath) {
+        itemStore.moveItemAtIndex(sourceIndexPath.row, toIndex: destinationIndexPath.row)
     }
 
 }
