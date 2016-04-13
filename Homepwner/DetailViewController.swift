64ad6a906fcf98f5b2202477f92ac98ac8cb97d1
Daniel Aquino <daniel.j.aquino@gmail.com>
2016-04-13 13:14:05 -0500

    Finished chapter 12.

---
+++ Homepwner/DetailViewController.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var serialField: UITextField!
+    @IBOutlet weak var valueField: UITextField!
+    @IBOutlet weak var dateLabel: UILabel!
+    var item: Item!
+    let numberFormatter: NSNumberFormatter = {
+        let formatter = NSNumberFormatter()
+        formatter.numberStyle = .DecimalStyle
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        return formatter
+    }()
+    
+    let dateFormatter: NSDateFormatter = {
+       let formatter = NSDateFormatter()
+        formatter.dateStyle = .MediumStyle
+        formatter.timeStyle = .NoStyle
+        return formatter
+    }()
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        nameField.text = item.name
+        serialField.text = item.serialNumber
+        valueField.text = numberFormatter.stringFromNumber(item.valueInDollars)
+        dateLabel.text = dateFormatter.stringFromDate(item.dateCreated)
+    }
+}
